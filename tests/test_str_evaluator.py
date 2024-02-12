46fde153126e49064ac5537d8bed4dd2c1ee052a
Razvan Pricop <22615594+RazorBest@users.noreply.github.com>
2024-02-13 01:04:54 +0200

    test: add len < 3 case

--- tests/test_str_evaluator.py
+++ tests/test_str_evaluator.py
@@ -2,5 +2,6 @@ from cdprecorder.str_evaluator import randomness_score
 
 
 def test_randomness_score():
+    assert randomness_score("xZi") == 0
     assert randomness_score("AAAAAAAAAAA") > 100
     assert randomness_score("ghfdsa7GADSUY8") < 50

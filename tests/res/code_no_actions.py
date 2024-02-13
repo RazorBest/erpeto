b72f119070023cfda14145d87a797aad304f6041
Razvan Pricop <22615594+RazorBest@users.noreply.github.com>
2024-02-14 00:47:12 +0200

    test: add resources for code generation testing

---
+++ tests/res/code_no_actions.py
@@ -0,0 +1,203 @@
+from __future__ import annotations
+import re
+import requests
+import urllib
+
+from requests import Session
+from abc import ABC, abstractmethod
+
+
+class LowercaseStr(str):
+    def __new__(cls, value: str, *args: object, **kwargs: object) -> LowercaseStr:
+        return super(LowercaseStr, cls).__new__(cls, value.lower(), *args, **kwargs)
+
+    def __eq__(self, other: object) -> bool:
+        return super().__eq__(str(other).lower())
+
+    def __hash__(self) -> int:
+        return super().__hash__()
+
+
+class Cookie:
+    def __init__(self, name = "", value = ""):
+        self.name = name
+        self.value = value
+
+    def to_dict(self):
+        return {self.name: self.value}
+
+    @classmethod
+    def list_from_cookiejar(cls, cookie_jar):
+        cookies = []
+        for cookie in cookie_jar:
+            obj = cls(
+                getattr(cookie, "name", ""),
+                getattr(cookie, "value", ""),
+            )
+            cookies.append(obj)
+
+        return cookies
+
+
+class RequestAction:
+    def __init__(self, method=None, url=None, headers=None, body=None, cookies=None, has_response=None):
+        self.method = method
+        self.url = url
+        self.headers = headers
+        self.cookies = cookies
+        self.body = body
+        self.has_response = has_response
+
+    def cookies_to_dict(self) -> dict[str, str]:
+        cookie_dict = {}
+        for cookie in self.cookies:
+            cookie_dict.update(cookie.to_dict())
+
+        return cookie_dict
+
+
+class ResponseAction:
+    def __init__(self, url=None, headers=None, body=None, cookies=None, status=None):
+        self.url = url
+        self.headers = headers
+        self.cookies = cookies
+        self.body = body
+        self.status = status
+
+
+class IntermediaryDataSource:
+    def __init__(self, upper_source):
+        self.upper_source = upper_source
+
+    def get_value(self, prev_actions):
+        upper_source_value = self.upper_source.get_value(prev_actions)
+        return get_value_from_upper_value(self.upper_source)
+
+
+class DataSource(ABC, ReprSource):
+    @abstractmethod
+    def get_value(self, prev_actions: Sequence[Optional[HttpAction]]) -> Optional[str]:
+        pass
+
+
+class IntermediaryDataSource:
+    def __init__(self, upper_source: DataSource) -> None:
+        self.upper_source = upper_source
+
+    def get_value(self, prev_actions: Sequence[Optional[HttpAction]]) -> Optional[str]:
+        upper_source_value = self.upper_source.get_value(prev_actions)
+        return self.get_value_from_upper_value(upper_source_value)
+
+    @abstractmethod
+    def get_value_from_upper_value(self, upper_value: str) -> Optional[str]:
+        pass
+
+
+class JSONSource(DataSource):
+    def __init__(self, data: str, token_classifier: Optional[Callable[[str], bool]] = None):
+        self.data = json.loads(data)
+
+        if token_classifier is not None:
+            self.classifier = token_classifier
+        else:
+            self.classifier = lambda token: randomness_score(token) >= 50
+
+        self.targets: list[JSONField] = []
+        self.classify_tokens()
+
+    def _classify(self, data: object, path: list[Union[str, int]]) -> list[JSONField]:
+        targets = []
+        if isinstance(data, dict):
+            for key, val in data.items():
+                targets += self._classify(val, path + [key])
+        elif isinstance(data, list):
+            for index, val in enumerate(data):
+                targets += self._classify(val, path + [index])
+        elif isinstance(data, str):
+            if self.classifier(data):
+                target = JSONField(data, path)
+                targets.append(target)
+        # TODO: decide for int, bool, bytes etc
+
+        return targets
+
+    def classify_tokens(self) -> None:
+        self.targets = self._classify(self.data, [])
+
+    def get_value(self, prev_actions: Sequence[Optional[HttpAction]]) -> str:
+        for field_target in self.targets:
+            if not field_target.source:
+                continue
+            field_target.apply(self.data, prev_actions)
+
+        return json.dumps(self.data)
+
+
+class StrSource(DataSource):
+    def __init__(self, text: str) -> None:
+        self.text = text
+
+    def get_value(self, prev_actions: Sequence[Optional[HttpAction]]) -> Optional[str]:
+        return self.text
+
+
+class BodyTarget:
+    def __init__(self, source: DataSource):
+        self.source = source
+
+    def apply(self, action: HttpAction, prev_actions: Sequence[Optional[HttpAction]]) -> None:
+        value = self.source.get_value(prev_actions)
+        if value is None:
+            return
+        action.body = value.encode()
+
+
+class CookieTarget:
+    def __init__(self, name: str, source: DataSource):
+        self.source = source
+        self.name = name
+
+    def apply(self, action: HttpAction, prev_actions: list[Optional[HttpAction]]) -> None:
+        value = self.source.get_value(prev_actions)
+        if value is None:
+            return
+        for cookie in action.cookies:
+            if cookie.name == self.name:
+                cookie.value = value
+                break
+        else:
+            action.cookies.append(Cookie(self.cookie.name, value))
+
+
+class HeaderTarget:
+    def __init__(self, source: DataSource, key: LowercaseStr, value: str):
+        self.source = source
+        self.key = key
+        self.value = value
+
+    def apply(self, action: HttpAction, prev_actions: Sequence[Optional[HttpAction]]) -> None:
+        value = self.source.get_value(prev_actions)
+        if value is None:
+            return
+        action.headers[self.key] = value
+
+
+def response_action_from_python_response(resp: requests.Response) -> ResponseAction:
+    headers: dict[LowercaseStr, str] = {}
+    for key, val in resp.headers.items():
+        # Ignore pseudo-headers: https://www.rfc-editor.org/rfc/rfc7540#section-8.1.2.1
+        if key.startswith(":"):
+            continue
+        headers[LowercaseStr(key)] = val
+
+    cookies = Cookie.list_from_cookiejar(resp.cookies)
+    return ResponseAction(
+        url=resp.url,
+        headers=headers,
+        cookies=cookies,
+        body=resp.raw,
+        status=resp.status_code,
+    )
+
+
+actions = []

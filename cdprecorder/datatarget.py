ab2ae022dffa9cf4110e27a42da67a1172aa7df0
Razvan Pricop <22615594+RazorBest@users.noreply.github.com>
2024-02-14 23:19:48 +0200

    feat: create abstract class for data targets

--- cdprecorder/datatarget.py
+++ cdprecorder/datatarget.py
@@ -1,46 +1,54 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
+from .action import LowercaseStr
+from cdprecorder.http_types import Cookie
+
 if TYPE_CHECKING:
-    from .action import HttpAction, LowercaseStr
+    from .action import HttpAction
     from .datasource import DataSource
 
-class CookieTarget:
-    def __init__(self, name: str, source: DataSource):
+
+class SingleSourcedTarget(ABC):
+    def __init__(self, source: DataSource):
         self.source = source
-        self.name = name
 
     def apply(self, action: HttpAction, prev_actions: list[Optional[HttpAction]]) -> None:
         value = self.source.get_value(prev_actions)
         if value is None:
             return
+        self.apply_value(action, value)
+
+    @abstractmethod
+    def apply_value(self, action: HttpAction, value: str) -> None:
+        pass
+
+
+class CookieTarget(SingleSourcedTarget):
+    def __init__(self, name: str, source: DataSource):
+        super().__init__(source)
+        self.name = name
+
+    def apply_value(self, action: HttpAction, value: str) -> None:
         for cookie in action.cookies:
             if cookie.name == self.name:
                 cookie.value = value
                 break
         else:
-            action.cookies.append(Cookie(self.cookie.name, value))
+            action.cookies.append(Cookie(self.name, value))
 
 
-class HeaderTarget:
-    def __init__(self, source: DataSource, key: LowercaseStr, value: str):
-        self.source = source
-        self.key = key
+class HeaderTarget(SingleSourcedTarget):
+    def __init__(self, source: DataSource, key: str, value: str):
+        super().__init__(source)
+        self.key = LowercaseStr(key)
         self.value = value
 
-    def apply(self, action: HttpAction, prev_actions: Sequence[Optional[HttpAction]]) -> None:
-        value = self.source.get_value(prev_actions)
-        if value is None:
-            return
+    def apply_value(self, action: HttpAction, value: str) -> None:
         action.headers[self.key] = value
 
 
-class BodyTarget:
-    def __init__(self, source: DataSource):
-        self.source = source
-
-    def apply(self, action: HttpAction, prev_actions: Sequence[Optional[HttpAction]]) -> None:
-        value = self.source.get_value(prev_actions)
-        if value is None:
-            return
+class BodyTarget(SingleSourcedTarget):
+    def apply_value(self, action: HttpAction, value:str) -> None:
         action.body = value.encode()

6bc6f30a164a9ef57cadabd31ef81990d21540a7
Razvan Pricop <22615594+RazorBest@users.noreply.github.com>
2024-02-15 11:38:37 +0200

    feat: add json_analyser

---
+++ cdprecorder/json_analyser.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+
+class JSONField:
+    def __init__(self, value: str, path: list[Union[str, int]], source: Optional[DataSource] = None):
+        """
+        Args:
+            path: The list of keys/indexes to use to arrive to a JSON element.
+        """
+        self.value = value
+        self.path = path
+        self.source = source
+
+
+class JSONSchema:
+    def __init__(self, data: str, token_classifier: Optional[Callable[[str], bool]] = None):
+        self.data = json.loads(data)
+
+        if token_classifier is not None:
+            self.classifier = token_classifier
+        else:
+            self.classifier = lambda token: randomness_score(token) >= 50
+
+        self.fields: list[JSONField] = []
+        self.classify_tokens()
+
+    def _classify(self, data: object, path: list[Union[str, int]]) -> list[JSONField]:
+        fields = []
+        if isinstance(data, dict):
+            for key, val in data.items():
+                fields += self._classify(val, path + [key])
+        elif isinstance(data, list):
+            for index, val in enumerate(data):
+                fields += self._classify(val, path + [index])
+        elif isinstance(data, str):
+            if self.classifier(data):
+                field = JSONField(data, path)
+                fields.append(field)
+        # TODO: decide for int, bool, bytes etc
+
+        return fields
+
+    def classify_tokens(self) -> None:
+        self.fields = self._classify(self.data, [])

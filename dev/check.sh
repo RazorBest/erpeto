50fe1dc487c79219b5d4db14034c7545e395b422
Razvan Pricop <22615594+RazorBest@users.noreply.github.com>
2024-02-16 23:15:22 +0200

    dev: make check.sh able to run separate checks

--- dev/check.sh
+++ dev/check.sh
@@ -7,9 +7,24 @@ set -o pipefail
 export NO_COLOR=false
 export PYTEST_ADDOPTS="--color=yes"
 
-python3 -m isort -c cdprecorder/
-python3 -m black --line-length 120 --check cdprecorder/
-python3 -m pylint --output-format=colorized cdprecorder/
-python3 -m mypy cdprecorder/
-python3 -m pytest --cov=cdprecorder tests
+args="$@"
+if [ "${args}" = "" ]; then
+    args="isort black pylint mypy pytest"
+fi
 
+for var in $args; do
+    if [ "${var}" = "isort" ]; then
+        python3 -m isort -c cdprecorder/
+    elif [ "${var}" = "black" ]; then
+        python3 -m black --line-length 120 --check cdprecorder/
+    elif [ "${var}" = "pylint" ]; then
+        python3 -m pylint --output-format=colorized cdprecorder/
+    elif [ "${var}" = "mypy" ]; then
+        python3 -m mypy cdprecorder/
+    elif [ "${var}" = "pytest" ]; then
+        python3 -m pytest --cov=cdprecorder tests
+    else
+        echo ${var}: Unkown check option
+    fi
+    #echo "$var"
+done
